ef8f4c7cb8928c93d96def9308b5d293cdc3819e
yaserrifai <yaserwrifai@gmail.com>
2022-06-30 17:33:03 +0200

    updatjs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 1- get data from URL(API)
 const api = "https://www.breakingbadapi.com/api/characters/";
 async function getData() {
     try {
@@ -6,66 +7,70 @@ async function getData() {
         console.log(data)
         printData(data)
     } catch (e) {
-        console.log("Error : ", e.message)
+        console.log("error : >>", e);
+
     }
 }
 
+// 2-print data in header and in content of Page
 function printData(data) {
-    const header = document.querySelector("#header")
-    const content = document.querySelector("#content")
-    const characterName = data.map((characther) => {
-        return characther.name
+    const select = document.getElementById("select");
+    data.forEach((element) => {
+            let myOption = document.createElement("option")
+            myOption.value = element.name
+            myOption.innerHTML = element.name
+            select.appendChild(myOption)
+                // show data into  cards
+
+            const mycard = document.getElementById("cardid");
+
+            let img = document.createElement("img");
+            img.setAttribute("src", element.img);
+            img.setAttribute("alt", "Not found");
+            img.classList.add("card-img-top");
+
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+
+            let h5 = document.createElement("h5");
+            cardBody.classList.add("card-title");
+            h5.innerHTML = element.name;
+
+            let p = document.createElement("p");
+            cardBody.classList.add("card-text");
+            p.innerHTML = element.birthady;
+
+            let myButton = document.createElement("a");
+
+
+
+            cardBody.appendChild(p);
+            cardBody.appendChild(h5);
+            cardBody.appendChild(myButton);
+
+            mycard.appendChild(cardBody);
+            mycard.appendChild(img);
+
+        }
+
+    );
+
+    select.addEventListener("change", (event) => {
+        console.log(event.target.value);
+        const charachterName = event.target.value
+
     })
-    header.innerHTML += `
-    <select class="form-control" onchange="getCh(this.value)">
-    <option>Please select an actor</option>
-    <option> ${charachter}</option> <select> `
-    const select = document.createElement("select")
-    select.classlist.add("form-control")
-    select.addEventListener("change", getCh(this.value))
-
-
 }
+// fetchCharacter(charachterName)
+
+
+//-----------------------------------
 async function getCh(name) {
     if (name != "Please select an actor") {
         const response = await fetch(`https://www.breakingbadapi.com/api/characters?name=${name}`);
         const data = await response.json();
-
-        /*  content.innerHTML=` < div class = "alert alert-success" > < h2 > $ { data[0].name }($ { data[0].nickname }) < /h2></div >
-        <
-        div class = "alert alert-danger" > < h4 > $ { data[0].portrayed } < /h4></div >
-        <
-        img class = "img-thumbnail"
-    src = "${data[0].img }"
-    width = "250" >
-        `*/
-
-        content.innerHTML = ` < div class = "card alert alert-secondary "
-    style = "width:350px" >
-        <
-        img class = "card-img-top img-thumbnail"
-    src = "${data[0].img }"
-    alt = "Card image"
-    style = "width:100%" / >
-        <
-        div class = "card-body" >
-        <
-        h4 class = "card-title alert alert-success" > ${ data[0].name }(${ data[0].nickname }) < /h4> <
-    h4 class = "alert-danger" > $ { data[0].portrayed } < /h4> <
-    a href = "#"
-    class = "btn btn-primary stretched-link" > See Profile < /a> */
-
-
-    <
-    /div> < /
-    div >
-
-
-
-
-        `
-
-
     }
+    return data.name
 }
+
 getData();
